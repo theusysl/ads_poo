80ecf4d685cf324177afa729ef41f79ca4f85b79
Matheus Paiva <theusysl@gmail.com>
2025-05-06 11:42:32 -0300

    aplicacao fullstack (2.5/3)

--- BackEndApp/src/java/api/TaskServlet.java
+++ BackEndApp/src/java/api/TaskServlet.java
@@ -1,3 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
 package api;
 
 import java.io.IOException;
@@ -8,24 +13,31 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
-import org.json.JSONObject;
+import java.util.ArrayList;
 import model.Task;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-@WebServlet(name="TasksServlet", urlPatterns={"/tasks"})
-public class TasksServlet extends HttpServlet {
+/**
+ *
+ * @author Matheus
+ */
+@WebServlet(name = "TaskServlet", urlPatterns = {"/taskss"})
+public class TaskServlet extends HttpServlet {
     
-    private JSONObject getJSONBody(BufferedReader reader ) throws Exception{
+    private JSONObject getJSONBody(BufferedReader reader) throws IOException{
         StringBuilder buffer = new StringBuilder();
         String line = null;
         while ((line = reader.readLine()) != null) {
             buffer.append(line);
         }
-        return new JSONObject (buffer.toString());
+        return new JSONObject(buffer.toString());
     }
 
-    /** 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -35,17 +47,18 @@ public class TasksServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
         JSONObject file = new JSONObject();
-        try{
+        try {
             file.put("list", new JSONArray(Task.list));
         }catch(Exception ex){
             response.setStatus(500);
             file.put("error", ex.getLocalizedMessage());
         }
         response.getWriter().print(file.toString());
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -55,7 +68,7 @@ public class TasksServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
         JSONObject file = new JSONObject();
-        try{
+        try {
             JSONObject body = getJSONBody(request.getReader());
             String title = body.getString("title");
             if(title != null){
@@ -70,18 +83,19 @@ public class TasksServlet extends HttpServlet {
         response.getWriter().print(file.toString());
     }
 
-    /** 
+    /**
      * Handles the HTTP <code>DELETE</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
      */
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
         JSONObject file = new JSONObject();
-        try{
+        try {
             String title = request.getParameter("title");
             int i = -1;
             for(Task t: Task.list){
@@ -90,15 +104,19 @@ public class TasksServlet extends HttpServlet {
                     break;
                 }
             }
-            if(i>-1) Task.list.remove(i);
+            if(i>=0) Task.list.remove(i);
             file.put("list", new JSONArray(Task.list));
         }catch(Exception ex){
-            response.setStatus(500);
+            response.setStatus(500); //Internal error
             file.put("error", ex.getLocalizedMessage());
         }
         response.getWriter().print(file.toString());
     }
 
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
--- BackEndApp/src/java/model/Task.java
+++ BackEndApp/src/java/model/Task.java
@@ -1,23 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
+
+/**
+ *
+ * @author Matheus
+ */
 
 import java.util.ArrayList;
 
-
 public class Task {
+    
     public static ArrayList<Task> list = new ArrayList<>();
     
     private String title;
-
-    public Task(String title) {
-        this.title = title;
+    
+    public Task() {
+        this.setTitle("[NEW]");
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    
+    public Task (String title) {
         this.title = title;
     }
     
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle (String title) {
+        this.title = title;
+    }
 }
